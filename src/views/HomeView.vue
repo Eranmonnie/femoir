9e0a6444488b3d2c076cdaf39f56beb217ba5071
Eranmonnie <ajalaferanmi123@gmail.com>
2024-05-26 15:11:39 +0100

    ,made home page responsive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,103 +1,110 @@
 <script setup></script>
 
 <template>
-  <div class="md:px-[6%] mt-10 mb-10">
-    <div class="flex gap-14 flex-col  lg:flex-row">
-      <div
-        class="overflow-hidden h-[430px] w-full lg:w-[60%]  border-none rounded-2xl"
-      >
+  <div class="px-[6%] mt-10 mb-10">
+
+    <div class="flex justify-between flex-col lg:flex-row">
+      <div class="w-full lg:w-[60%] border-none rounded-2xl">
         <img
-          class="w-full h-full"
-          src="/assets/istockphoto-583809524-612x612.jpg"
+          class="w-full lg:h-full rounded-2xl object-contain"
+          src="/assets/32fbf500069ff28a28fa903158cf2dada613a9e3_high.webp"
         />
       </div>
 
-      <div class="w-full lg:w-[30%]  ">
+      <div class="w-full lg:w-[34%] p-0 ">
         <h3 class="text-xl font-roboto-regular">Other featured posts</h3>
 
-        <div class="flex gap-5 lg:flex-col lg:gap-0">
-          <div class="flex flex-col lg:flex-row gap-2 mb-4 mt-4">
+        <div
+          class="flex flex-col lg:flex-row lg:flex-wrap w-full gap-5 lg:gap-0"
+        >
+          <div class="flex flex-col lg:flex-row gap-2 my-3 w-full lg:w-auto">
             <img
               src="/assets/istockphoto-583809524-612x612.jpg"
-              class="lg:w-16 lg:h-[45px]  rounded-lg"
+              class="lg:w-16 lg:h-[45px] rounded-lg"
               alt=""
             />
-            <div class="p-1">
-              <div class="text-sm font-roboto-regular">
-                Revolutionazing indusrties
-              </div>
-              <div class="text-sm font-roboto-regular">
-                Revolutionazing indusrties
-              </div>
-            </div>
-          </div>
-          <hr class="hidden lg:inline-block"/>
-          <div class="flex flex-col lg:flex-row gap-2 mb-4 mt-4">
-            <img
-              src="/assets/istockphoto-583809524-612x612.jpg"
-              class="lg:w-16 lg:h-[45px]  rounded-lg"
-              alt=""
-            />
-            <div class="p-1">
-              <div class="text-sm font-roboto-regular">
-                Revolutionazing indusrties
-              </div>
-              <div class="text-sm font-roboto-regular">
-                Revolutionazing indusrties
-              </div>
-            </div>
-          </div>
-          <hr class="hidden lg:inline-block"/>
-          <div class="flex flex-col lg:flex-row gap-2 mb-4 mt-4">
-            <img
-              src="/assets/istockphoto-583809524-612x612.jpg"
-              class="lg:w-16 lg:h-[45px]  rounded-lg"
-              alt=""
-            />
-            <div class="p-1">
-              <div class="text-sm font-roboto-regular">
-                Revolutionazing indusrties
-              </div>
-              <div class="text-sm font-roboto-regular">
-                Revolutionazing indusrties
-              </div>
-            </div>
-          </div>
-          <hr class="hidden lg:inline-block"/>
-          <div class="flex flex-col lg:flex-row gap-2 mb-4 mt-4">
-            <img
-              src="/assets/istockphoto-583809524-612x612.jpg"
-              class="lg:w-16 lg:h-[45px]  rounded-lg"
-              alt=""
-            />
-            <div class="p-1">
-              <div class="text-sm font-roboto-regular">
-                Revolutionazing indusrties
-              </div>
-              <div class="text-sm font-roboto-regular">
-                Revolutionazing indusrties
-              </div>
-            </div>
-          </div>
-          <hr class="hidden lg:inline-block"/>
-          <div class="flex flex-col lg:flex-row gap-2 mb-4 mt-4">
-            <img
-              src="/assets/istockphoto-583809524-612x612.jpg"
-              class="lg:w-16 lg:h-[45px]  rounded-lg"
-              alt=""
-            />
-            <div class="p-1">
-              <div class="text-sm font-roboto-regular">
-                Revolutionazing indusrties
-              </div>
-              <div class="text-sm font-roboto-regular">
-                Revolutionazing indusrties
-              </div>
-            </div>
-          </div>
-          <hr class="hidden lg:inline-block"/>
 
-          
+            <div class="p-1">
+              <div class="text-sm font-roboto-regular">
+                Revolutionizing industries
+              </div>
+              <div class="text-sm font-roboto-regular">
+                Revolutionizing industries
+              </div>
+            </div>
+          </div>
+
+          <hr class="hidden lg:block w-full" />
+          <div class="flex flex-col lg:flex-row gap-2 my-3 w-full lg:w-auto">
+            <img
+              src="/assets/istockphoto-583809524-612x612.jpg"
+              class="lg:w-16 lg:h-[45px] rounded-lg"
+              alt=""
+            />
+
+            <div class="p-1">
+              <div class="text-sm font-roboto-regular">
+                Revolutionizing industries
+              </div>
+              <div class="text-sm font-roboto-regular">
+                Revolutionizing industries
+              </div>
+            </div>
+          </div>
+
+          <hr class="hidden lg:block w-full" />
+          <div class="flex flex-col lg:flex-row gap-2 my-3 w-full lg:w-auto">
+            <img
+              src="/assets/istockphoto-583809524-612x612.jpg"
+              class="lg:w-16 lg:h-[45px] rounded-lg"
+              alt=""
+            />
+
+            <div class="p-1">
+              <div class="text-sm font-roboto-regular">
+                Revolutionizing industries
+              </div>
+              <div class="text-sm font-roboto-regular">
+                Revolutionizing industries
+              </div>
+            </div>
+          </div>
+
+          <hr class="hidden lg:block w-full" />
+          <div class="flex flex-col lg:flex-row gap-2 my-3 w-full lg:w-auto">
+            <img
+              src="/assets/istockphoto-583809524-612x612.jpg"
+              class="lg:w-16 lg:h-[45px] rounded-lg"
+              alt=""
+            />
+
+            <div class="p-1">
+              <div class="text-sm font-roboto-regular">
+                Revolutionizing industries
+              </div>
+              <div class="text-sm font-roboto-regular">
+                Revolutionizing industries
+              </div>
+            </div>
+          </div>
+
+          <hr class="hidden lg:block w-full" />
+          <div class="flex flex-col lg:flex-row gap-2 my-3 w-full lg:w-auto">
+            <img
+              src="/assets/istockphoto-583809524-612x612.jpg"
+              class="lg:w-16 lg:h-[45px] rounded-lg"
+              alt=""
+            />
+
+            <div class="p-1">
+              <div class="text-sm font-roboto-regular">
+                Revolutionizing industries
+              </div>
+              <div class="text-sm font-roboto-regular">
+                Revolutionizing industries
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -110,8 +117,10 @@
     </div>
   </div>
 
-  <div class="flex gap-10 px-[6%] justify-center font-roboto-regular">
-    <div class="w-[22%] flex justify-center flex-col gap-2">
+  <div
+    class="grid gap-10 px-[6%] font-roboto-regular grid-cols-1 sm:grid-cols-2 lg:grid-cols-4"
+  >
+    <div class="flex justify-center flex-col gap-2">
       <img
         src="/assets/istockphoto-583809524-612x612.jpg"
         class="w-full rounded-lg"
@@ -126,23 +135,7 @@
       </div>
     </div>
 
-    <div class="w-[22%] flex justify-center flex-col gap-2">
-      <img
-        src="/assets/istockphoto-583809524-612x612.jpg"
-        class="w-full rounded-lg"
-        alt=""
-      />
-
-      <div class="text-[20px]">
-        Mastering Ui elements: A pratical guide for designers
-      </div>
-      <div class="text-[13px]">
-        Dive into the world of user interfece with our experf guide latest
-        treand and practical tips
-      </div>
-    </div>
-
-    <div class="w-[22%] flex justify-center flex-col gap-2">
+    <div class="flex justify-center flex-col gap-2">
       <img
         src="/assets/istockphoto-583809524-612x612.jpg"
         class="w-full rounded-lg"
@@ -157,7 +150,22 @@
       </div>
     </div>
 
-    <div class="w-[22%] flex justify-center flex-col gap-2">
+    <div class="flex justify-center flex-col gap-2">
+      <img
+        src="/assets/istockphoto-583809524-612x612.jpg"
+        class="w-full rounded-lg"
+        alt=""
+      />
+      <div class="text-[20px]">
+        Mastering Ui elements: A pratical guide for designers
+      </div>
+      <div class="text-[13px]">
+        Dive into the world of user interfece with our experf guide latest
+        treand and practical tips
+      </div>
+    </div>
+
+    <div class="flex justify-center flex-col gap-2">
       <img
         src="/assets/istockphoto-583809524-612x612.jpg"
         class="w-full rounded-lg"
